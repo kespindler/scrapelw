238c9658d767b0c83ba233b3d252ff4c781d3b71
Kurt <kespindler@gmail.com>
2013-07-07 14:32:14 +0200

    fixed the scraping of How to Actually Change your Mind

--- scrapeLW.py
+++ scrapeLW.py
@@ -43,51 +43,59 @@ def parse_article(url):
     html = lxml.tostring(body)
     return title, html
 
-hotstart = None#'How_To_Actually_Change_Your_Mind' #only works on top level at the moment
+hotstart = 'How_To_Actually_Change_Your_Mind' #only works on top level at the moment
 def get_links_from_toc(url, section, path):
     if not url.startswith('http'):
         url = 'http://' + DOMAIN + url
     print url
     global hotstart
     tree = lxml.parse(url, parser=PARSER)
-    ul = tree.xpath('//table[@id="toc"]/tr/td/ul/li[%d]/ul' % (section,))[0]
-    titles = [li[0].get('href') for li in ul.getchildren()]
-    if 1:
-        for i, t in enumerate(titles): 
-            title = t[1:]
-            if hotstart:
-                if title != hotstart:
-                    continue
-                else:
-                    hotstart = None
-            a = tree.xpath('//span[@id="'+title+'"]/a')[0]
-            newurl = a.get('href')
-            if not urlparse(newurl).netloc:
-                fpath = os.path.join(path, '%02d'%i)
-                with open(os.path.join(path, 'titles.txt'), 'a') as f:
-                    f.write(title + '\n')
-                os.mkdir(fpath)
-                print 'Exploring', title
-                try:
-                    url = 'http://' + urlparse(url).netloc + newurl
-                    get_links_from_toc(url, 1, fpath)
-                except Exception as e:
-                    print 'Failed on3', newurl
-                    print e
-            else: # assumed to be 'lesswrong.com'
-                #try:
-                if 1:
-                    title, html = parse_article(newurl)
-                    #fname = "".join(x for x in title if x.isalnum())
-                    with open(os.path.join(path, '%02d.html'%i), 'w') as f:
-                        f.write(html)
-                    with open(os.path.join(path, 'titles.txt'), 'a') as f:
-                        f.write(title + '\n')
-                    print 'Stored', title
-                #except:
-                #    print 'Failed on2', newurl
-    #except:
-    #    print "failed on", url
+    ul = tree.xpath('//table[@id="toc"]/tr/td/ul/li[%d]/ul' % (section,))
+    if ul:
+        titles = [li[0].get('href')[1:] for li in ul[0].getchildren()]
+        links = [tree.xpath('//span[@id="'+title+'"]/a') for title in titles]
+        links = [l[0] for l in links if l]
+        urls = [a.get('href') for a in links]
+        #print '\n'.join(str(x) for x in zip(titles, links, urls))
+    else: 
+        header = tree.xpath('//span[@id="Blog_posts"]')
+        if header:
+            ul = header[0].getparent().getnext()
+            titles = [l[0].text for l in ul]
+            urls = [l[0].get('href') for l in ul]
+        else:
+            print "No links to follow", url
+
+    #print titles
+    for i, title in enumerate(titles): 
+        if hotstart:
+            if title != hotstart:
+                continue
+            hotstart = None
+        #print i, title
+        newurl = urls[i]
+        if not urlparse(newurl).netloc:
+            fpath = os.path.join(path, '%02d'%i)
+            with open(os.path.join(path, 'titles.txt'), 'a') as f:
+                f.write(title + '\n')
+            os.mkdir(fpath)
+            print 'Exploring', title
+          #  try:
+            if 1:
+                url = 'http://' + urlparse(url).netloc + newurl
+                get_links_from_toc(url, 1, fpath)
+            #except Exception as e:
+            #    print 'Failed on3', newurl
+            #    print e
+        else: # assumed to be 'lesswrong.com'
+            #try:
+            title, html = parse_article(newurl)
+            #fname = "".join(x for x in title if x.isalnum())
+            with open(os.path.join(path, '%02d.html'%i), 'w') as f:
+                f.write(html)
+            with open(os.path.join(path, 'titles.txt'), 'a') as f:
+                f.write(title + '\n')
+            print 'Stored', title
 
 get_links_from_toc('http://'+DOMAIN + '/wiki/Sequences', 4, outdir)
 print 'Complete.'
